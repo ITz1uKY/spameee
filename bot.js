734c6822afb27f9b9778eb8cbccc0278a93dce29
ITz1uKY <42788278+ITz1uKY@users.noreply.github.com>
2018-11-26 23:46:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "1";
+var prefix = "5";
 
 
 
@@ -23,6 +23,21 @@ var channel = "512699464302198810";//ايدي الروم
  
 
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
 
 
 
